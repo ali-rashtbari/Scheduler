c1668376950ae0abd6ac6b5ecbf8960eda267617
AAA <dev.alirashtbari@gmail.com>
2023-12-29 18:55:40 +0330

    init

---
+++ SchedulerOnConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using SchedulerOnConsole.Services;
+
+Console.WriteLine("Press any key to start Taks ...");
+Console.ReadKey();
+
+//var task1 = new BackgroundTask1(TimeSpan.FromMilliseconds(1000));
+var task2 = new BackgroundTask2(TimeSpan.FromMilliseconds(1000));
+
+//task1.Start();
+task2.Start();
+
+Console.WriteLine("Press any button to stop the Task ... ");
+Console.ReadKey();
+
+//await task1.StopAsync();
+await task2.StopAsync();
---
+++ SchedulerOnConsole/Services/BackgroundTask1.cs
@@ -0,0 +1,105 @@
+﻿namespace SchedulerOnConsole.Services
+{
+    public class BackgroundTask1
+    {
+        private Task? _timerTask;
+        private readonly PeriodicTimer _timer;
+        private readonly CancellationTokenSource _cancellationTokenSource = new();
+
+        public BackgroundTask1(TimeSpan interval)
+        {
+            _timer = new PeriodicTimer(interval);
+        }
+
+        public void Start()
+        {
+            _timerTask = DoWorkAsync1();
+        }
+
+        private async Task DoWorkAsync1()
+        {
+            try
+            {
+                while(await _timer.WaitForNextTickAsync(_cancellationTokenSource.Token)) // درستش اینه که سعی میکنه زمان رو نزدیک به زمان استارت نگه داره و در طولانی مدت باعث افزایش اخلاف نشه ---
+                {
+                    Console.WriteLine($"DoWork-1: {DateTime.Now:O}");
+                }
+
+                //while (true) // این کاملا اشتباهه ---
+                //{
+                //    Console.WriteLine(DateTime.Now:O);
+                //    Thread.Sleep(1000);
+                //}
+
+            }
+            catch (OperationCanceledException ex)
+            {
+                throw;
+            }
+        }
+
+
+        public async Task StopAsync()
+        {
+            if (_timer is null) return;
+            _cancellationTokenSource.Cancel();
+            await _timerTask;
+            _cancellationTokenSource.Dispose();
+            Console.WriteLine("Task was Cancelled :)");
+        }
+
+    }
+
+
+    public class BackgroundTask2
+    {
+        private Task? _timerTask;
+        private readonly PeriodicTimer _timer;
+        private readonly CancellationTokenSource _cancellationTokenSource = new();
+
+        public BackgroundTask2(TimeSpan interval)
+        {
+            _timer = new PeriodicTimer(interval);
+        }
+
+        public async void Start()
+        {
+            _timerTask = DoWorkAsync2();
+        }
+
+        private async Task DoWorkAsync2()
+        {
+            try
+            {
+                //while (await _timer.WaitForNextTickAsync(_cancellationTokenSource.Token)) // درستش اینه که سعی میکنه زمان رو نزدیک به زمان استارت نگه داره و در طولانی مدت باعث افزایش اخلاف نشه ---
+                //{
+                //    Console.WriteLine($"DoWork-2: {DateTime.Now:O}");
+                //}
+
+                while (true) // این کاملا اشتباهه ---
+                {
+                    Console.WriteLine($"DoWork-2: {DateTime.Now:O}");
+                    await Task.Delay(1000);
+                }
+
+            }
+            catch (OperationCanceledException ex)
+            {
+                throw;
+            }
+        }
+
+
+        public async Task StopAsync()
+        {
+            if (_timer is null) return;
+            _cancellationTokenSource.Cancel();
+            await _timerTask;
+            _cancellationTokenSource.Dispose();
+            Console.WriteLine("Task was Cancelled :)");
+        }
+
+    }
+
+
+}
